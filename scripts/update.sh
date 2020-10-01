4170b9ee3d4b39f25ec42eaf99d3ef6f33fff119
Giovanni Tirloni <giovanni.tirloni@gmail.com>
2020-10-01 13:03:47 -0300

    Remove unnecessary update.sh script

--- scripts/update.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -eux
-
-echo "==> Applying updates"
-yum -y update
-
-echo "==> Rebooting the machine..."
-systemctl stop sshd.service
-nohup shutdown -r now < /dev/null > /dev/null 2>&1 &
-exit 0
